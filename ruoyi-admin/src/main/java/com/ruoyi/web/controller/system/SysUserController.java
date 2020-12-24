178cb8786d4b6815d2924cd561d9949f6541ff59
xumin <xumin@glaway.com>
2020-12-24 17:34:24 +0800

    前端和后端修改2

--- ruoyi-admin/src/main/java/com/ruoyi/web/controller/system/SysUserController.java
+++ ruoyi-admin/src/main/java/com/ruoyi/web/controller/system/SysUserController.java
@@ -1,6 +1,9 @@
 package com.ruoyi.web.controller.system;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import com.ruoyi.system.domain.User;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
